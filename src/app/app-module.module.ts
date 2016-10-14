afeb24a86c4d106c1f96a958721d552fd2f95f94
JWA111 <jwadam11@asu.edu>
2016-10-14 13:59:15 -0700

    Remove reference to sidnav

--- src/app/app-module.module.ts
+++ src/app/app-module.module.ts
@@ -5,7 +5,6 @@ import { MdCardModule } from '@angular2-material/card';
 import { MdListModule } from '@angular2-material/list';
 import { MdProgressBarModule } from '@angular2-material/progress-bar';
 import { MdRadioModule } from '@angular2-material/radio';
-import { MdSidenavModule } from '@angular2-material/sidenav';
 import { MdToolbarModule } from '@angular2-material/toolbar';
 import { ModalModule  } from 'ng2-bootstrap/ng2-bootstrap';
 import { SidebarModule } from 'ng2-sidebar';
@@ -28,7 +27,6 @@ import { ChallengeService, DocumentService } from './services';
     MdListModule.forRoot(),
     MdProgressBarModule.forRoot(),
     MdRadioModule.forRoot(),
-    MdSidenavModule.forRoot(),
     MdToolbarModule.forRoot(),
     ModalModule,
     SidebarModule
