0cd266698a355a71239d1a096f41ea3d6afa73ad
JWA111 <jwadam11@asu.edu>
2016-10-18 14:39:56 -0700

    Fix sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,24 +32,19 @@ export class AppComponent implements OnInit {
     // this.helpModal.show();
   }
 
-  private _toggleSidebar() {
+  public toggleSidebar(): void {
     this._open = !this._open;
-    if (!this._open) {
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("main").style.marginLeft = "0";
-      document.body.style.backgroundColor = "white";
-    } else {
-      document.getElementById("mySidenav").style.width = "250px";
-      document.getElementById("main").style.marginLeft = "250px";
-      document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    }
   }
 
-   private _closeOnClickOutside(){
-    this.closeOnClickOutside = !this.closeOnClickOutside;
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
+  public onSidebarOpen(): void {
+    document.getElementById('mySidenav').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+    document.body.style.backgroundColor = 'rgba(0,0,0,0.4)';
+  }
+
+   public onSidebarClose(): void {
+    document.getElementById('mySidenav').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+    document.body.style.backgroundColor = 'white';
    }
-  
 }
