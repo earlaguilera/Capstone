9b745f430c4bd29d064dd340b2151db8825cff9e
JamesAdams11 <jwadam11@asu.edu>
2016-12-01 17:08:10 -0700

    add mock screens, fix menu

--- server.js
+++ server.js
@@ -8,8 +8,20 @@ app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/dist/index.html'))
-})
+});
+
+app.get('/profile', function (req, res) {
+  res.sendFile(path.join(__dirname+'/dist/user-profile.html'))
+});
+
+app.get('/instructor', function (req, res) {
+  res.sendFile(path.join(__dirname+'/dist/instructor.html'))
+});
+
+app.get('/social', function (req, res) {
+  res.sendFile(path.join(__dirname+'/dist/social.html'))
+});
 
 app.listen(port, function () {
   console.log('App listening on port ' + port);
-})
+});
