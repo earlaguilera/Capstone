88169c671e7578e77f01ab0030cafa8f6804015a
Kartik Gupta <kartikg95@yahoo.com>
2017-11-05 08:12:30 -0700

    Removed Grade ~ Added call to Grade Model

--- src/app/models/facilitator.model.ts
+++ src/app/models/facilitator.model.ts
@@ -1,15 +1,10 @@
 import { Student } from './student.model';
 import { Task } from './task.model';
+import { Gradde } from './grade.model';
 
 export interface Facilitator {
     id: string;
     students: Student[];
 	grades: Grades[];
 	tasks: Task[];
-}
-
-export interface Grades{
-	student: string;
-	task_id: string;
-	grade: string;
 }
